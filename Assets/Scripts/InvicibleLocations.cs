16a358ab777f05267890edb5d0b99df9f7a53926
NaichuLy <naichu2612@gmail.com>
2025-06-13 02:43:34 -0300

    Particulas
    Particulas de muerte y de cercania con las cosas invisibles

--- Assets/Scripts/InvicibleLocations.cs
+++ Assets/Scripts/InvicibleLocations.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class InvicibleLocations : MonoBehaviour
 {
     [SerializeField] private LayerMask _invisibleRayMask;
-    [SerializeField] private float _visionRadius = 4f; 
-    private Vector3 _posOffset = new(0, 1f, 0); 
-    private bool _isNear;
+    [SerializeField] private float _visionRadius = 4f;
+    private Vector3 _posOffset = new(0, 1f, 0);
+    private ParticleSystem _alertParticles;
+    private bool _isNear = false;
+    private bool _wasNear = false;
+
+    private void Start()
+    {
+        _alertParticles = GetComponentInChildren<ParticleSystem>();
+    }
 
     private void Update()
     {
-        _isNear = IsNear();
-        if (_isNear)
+        _isNear = IsNearInvisibleSpikes();
+
+        if (_isNear && !_wasNear)
         {
-            Debug.Log("Hay algo invisible cerca");
+            Debug.Log("particulas si");
+            _alertParticles.Play();
         }
-        else
+        else if (!_isNear && _wasNear)
         {
-            Debug.Log("No hay algo invisible cerca");
+            Debug.Log("particulas no");
+            _alertParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
         }
+
+        _wasNear = _isNear;
     }
 
-    private bool IsNear()
+    private bool IsNearInvisibleSpikes()
     {
         Vector3 sphereCenter = transform.position + _posOffset;
         return Physics.CheckSphere(sphereCenter, _visionRadius, _invisibleRayMask);
--- Assets/Scripts/Life/PlayerHealth.cs
+++ Assets/Scripts/Life/PlayerHealth.cs
@@ -11,20 +11,18 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] GameObject happyFace;
     [SerializeField] GameObject neutralFace;
     [SerializeField] GameObject deadFace;
+    [SerializeField] private GameObject _antenaRenderer;
+    [SerializeField] private Renderer _bodyRenderer;
+    [SerializeField] private ParticleSystem _deathParticles;
+    private ParticleSystem _alertParticles;
 
     private void Start()
     {
         _playerLife = _maxPlayerLife;
         happyFace.SetActive(true);
+        _alertParticles = GetComponentInChildren<ParticleSystem>();
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            GetDamage(4);
-        }
-    }
     public void GetDamage(float damage)
     {
         _playerLife -= damage;
@@ -34,8 +32,12 @@ public class PlayerHealth : MonoBehaviour
 
         if (_playerLife <= 0)
         {
-            Destroy(gameObject, 0.3f);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            GetComponent<MovementPlayer>()._moveSpeed = 0f;
+            _alertParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            _antenaRenderer.SetActive(false);
+            _bodyRenderer.enabled = false;
+            _deathParticles.Play();
+            Invoke("ResetScene", 0.7f);
         }
     }
 
@@ -59,6 +61,12 @@ public class PlayerHealth : MonoBehaviour
             neutralFace.SetActive(false);
             deadFace.SetActive(true);
         }
+    }
+
+    private void ResetScene()
+    {
+       Destroy(gameObject, 0.3f);
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
